cfcc1b6cb31bf3682f12cabef2a811e3c22c9352
Юрий Вербитский 24 <000333555666@mail.ru>
2022-10-11 17:51:17 +0800

    Вычитание из классов

---
+++ 2.1/11.10 - Вычитание/vichitalka.py
@@ -0,0 +1,8 @@
+﻿class Spysok(list):
+    def __init__(self, *args):
+        super(Spysok, self).__init__(args)
+    def __sub__(self, other):
+        return self.__class__(*[item for item in self if item not in other])
+x = Spysok(7, 8, 9, 77, 52)
+y = Spysok(25, 5, 2, 7, 8)
+print(x - y)
